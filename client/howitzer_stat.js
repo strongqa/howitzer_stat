a54b6958de72e4106da716096c127a4af5ea037a
Roman Parashenko <romikoops@gmail.com>
2014-01-25 02:36:54 +0200

    Fix AJAX requests and layout

--- client/howitzer_stat.js
+++ client/howitzer_stat.js
@@ -2,7 +2,7 @@
     // method to make AJAX request to web-service
     function xdr(method, data, callback, errback) {
         var req, url;
-        var serviceDomain = "http://localhost:7000"; // add here domain of web-service where howitzer_stat is working
+        var serviceDomain = "http://checklist.reportdrop.net:8001"; // add here domain of web-service where howitzer_stat is working
 
         if (data.pageName) {
             url = serviceDomain + "/pages/" + data.pageName;
@@ -44,62 +44,50 @@
     var errorHandler = function(error){
         alert(error);
     };
-    var pageClassesByTitleAndUrl;
-    var pageClassesByTitleAndUrlHandler = function(data){
-        pageClassesByTitleAndUrl = JSON.parse(data).page;
-    };
-    var featuresByClassName;
-    var featuresByClassNameHandler = function(data){
-        featuresByClassName = JSON.parse(data).features;
-    };
 
-    xdr('GET', {"url": document.url, "title": document.title}, pageClassesByTitleAndUrlHandler, errorHandler);
-    xdr('GET', {"pageName": pageClassesByTitleAndUrl[0]}, featuresByClassNameHandler, errorHandler);
+    var pageClassesByTitleAndUrlHandler = function(data) {
+        var pageName = JSON.parse(data).page[0];
 
-    var pagesDiv; //main stats container
-    var popupDiv; //popup container
+        var statButton = document.getElementById('howitzer_stat_btn');
 
-    // event listener to open and close popup
-    function popupHandler() {
-        if (popupDiv.style.display == 'none') {
-            popupDiv.setAttribute('style','display: block');
-            this.setAttribute('class', 'stat-button active');
-        } else {
-            popupDiv.setAttribute('style','display: none');
-            this.setAttribute('class', 'stat-button non-active');
-        }
-    }
-    // template creating
-    window.onload = function () {
-        pagesDiv = document.getElementById('stat-result');
-
-        var activeClassButton = document.createElement('div');
-        activeClassButton.setAttribute('class', 'stat-button non-active');
-
-        var classNameSpan = document.createElement('span');
-        classNameSpan.setAttribute('class', 'stat-number');
-        if (pageClassesByTitleAndUrl[0] == null) {
-            classNameSpan.innerHTML = 'Unknown Page';
-            activeClassButton.addEventListener('click', function () {
+        var pageNameElement = document.getElementById('howitzer_stat_page_name');
+        if (pageName == null) {
+            pageNameElement.innerHTML = 'Unknown Page';
+            statButton.addEventListener('click', function () {
                 alert("Page is Unknown, stats can't be loaded.")
             });
         } else {
-            classNameSpan.innerHTML = pageClassesByTitleAndUrl[0];
-            activeClassButton.addEventListener('click', popupHandler);
+            pageNameElement.innerHTML = pageName;
+            pageNameElement.setAttribute('data-page-name', pageName);
+            statButton.addEventListener('click', function () {
+                if (this.className.indexOf('non-active') >= 0) {
+                    if (document.getElementById('stat-info').innerHTML == ''){
+                        pageNameElement.innerHTML = 'Loading...';
+                        xdr('GET', {"pageName": pageName}, featuresByClassNameHandler, errorHandler);
+                    } else {
+                        document.getElementById('stat-popup').setAttribute('style','display: block;');
+                        this.className = this.className.replace('non-active', 'active');
+                    }
+                } else {
+                    document.getElementById('stat-popup').setAttribute('style','display: none;');
+                    this.className = this.className.replace('active', 'non-active');
+                }
+            });
         }
-        activeClassButton.appendChild(classNameSpan);
-        pagesDiv.appendChild(activeClassButton);
+        statButton.setAttribute('style', 'display: block;');
+    };
 
-        var scriptTag = document.getElementById('cucumberStat');
-        popupDiv = document.getElementById('stat-popup');
+    var featuresByClassNameHandler = function(data){
+        var template = document.getElementById('cucumberStat').innerHTML;
+        document.getElementById('stat-info').innerHTML = _.template(template, {features: JSON.parse(data).features});
+        document.getElementById('stat-popup').setAttribute('style','display: block;');
+        var statButton = document.getElementById('howitzer_stat_btn');
+        statButton.className = statButton.className.replace('non-active', 'active');
+        var pageNameElement = document.getElementById('howitzer_stat_page_name');
+        pageNameElement.innerHTML = pageNameElement.getAttribute('data-page-name');
+    };
 
-        var target = document.createElement('div');
-        target.setAttribute('id', 'stat-info');
-
-        target.innerHTML = _.template(scriptTag.innerHTML, {features: featuresByClassName});
-
-        popupDiv.appendChild(target);
-        pagesDiv.appendChild(popupDiv);
-        popupDiv.setAttribute('style', 'display: none');
+    window.onload = function () {
+        xdr('GET', {"url": document.url, "title": document.title}, pageClassesByTitleAndUrlHandler, errorHandler);
     };
 })();
